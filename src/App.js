bda02b2343cfdb028baa56f70f155cff34d3852d
Iqbal Fasri <iqbalx64@gmail.com>
2019-07-14 20:43:36 +0700

    Use stateless component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // Import Css
 import './App.scss';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="contain">
-                <h1>Welcome to Basic React Parcel</h1>
-                <p>Edit <code>./src/App.js</code></p>
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div className="contain">
+            <h1>Welcome to Basic React Parcel</h1>
+            <p>Edit <code>./src/App.js</code></p>
+        </div>
+    );
 }
 
 export default App;
